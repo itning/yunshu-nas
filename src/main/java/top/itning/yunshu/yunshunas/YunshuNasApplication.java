855d1c81cb2b8422d4772a29802fba0aa1e06f7e
itning <itning@itning.top>
2019-07-17 21:41:45 +0800

    log

--- src/main/java/top/itning/yunshu/yunshunas/YunshuNasApplication.java
+++ src/main/java/top/itning/yunshu/yunshunas/YunshuNasApplication.java
@@ -2,8 +2,10 @@ package top.itning.yunshu.yunshunas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class YunshuNasApplication {
 
     public static void main(String[] args) {
