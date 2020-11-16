a96b8b24608e398a375a2c8ac6e1cad518cdbd4f
itning <ning.wang8@geely.com>
2020-11-16 11:11:10 +0800

    refactor: cors

--- nas-deploy/src/main/java/top/itning/yunshunas/deploy/config/CustomWebMvcConfig.java
+++ nas-deploy/src/main/java/top/itning/yunshunas/deploy/config/CustomWebMvcConfig.java
@@ -1,8 +1,11 @@
 package top.itning.yunshunas.deploy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 /**
  * @author itning
@@ -12,10 +15,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CustomWebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] allMethod = Arrays.stream(HttpMethod.values()).map(Enum::name).toArray(String[]::new);
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .allowedMethods(allMethod)
                 .maxAge(86400);
     }
 }
