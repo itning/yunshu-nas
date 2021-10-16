085e431cdb27b8283ed87976d774bca87668e4d1
itning <itning@itning.top>
2021-10-16 09:42:24 +0800

    docs: 补充文档注释

--- nas-music/src/main/java/top/itning/yunshunas/music/controller/FileController.java
+++ nas-music/src/main/java/top/itning/yunshunas/music/controller/FileController.java
@@ -12,6 +12,8 @@ import javax.validation.constraints.NotNull;
 import java.io.IOException;
 
 /**
+ * 音乐文件相关接口
+ *
  * @author itning
  * @date 2019/7/14 18:48
  */
@@ -26,6 +28,15 @@ public class FileController {
         this.fileService = fileService;
     }
 
+    /**
+     * 获取音乐
+     *
+     * @param range    {@link org.springframework.http.HttpHeaders#RANGE}
+     * @param id       歌曲ID
+     * @param request  {@link HttpServletRequest}
+     * @param response {@link HttpServletResponse}
+     * @throws Exception 获取失败
+     */
     @GetMapping
     public void getMusic(@RequestHeader(required = false) String range,
                          @NotNull(message = "ID不存在") String id,
@@ -34,12 +45,28 @@ public class FileController {
         fileService.getOneMusic(id, range, request, response);
     }
 
+    /**
+     * 获取歌词
+     *
+     * @param id 歌词ID
+     * @return 歌词
+     * @throws IOException 歌词获取失败
+     */
     @ResponseBody
     @GetMapping(value = "/lyric", produces = MediaType.TEXT_PLAIN_VALUE)
     public String getLyric(@NotNull(message = "ID不存在") String id) throws IOException {
         return fileService.getLyric(id);
     }
 
+    /**
+     * 获取封面
+     *
+     * @param range    {@link org.springframework.http.HttpHeaders#RANGE}
+     * @param id       歌曲ID
+     * @param request  {@link HttpServletRequest}
+     * @param response {@link HttpServletResponse}
+     * @throws Exception 获取失败
+     */
     @GetMapping("/cover")
     public void metaInfo(@RequestHeader(required = false) String range,
                          @NotNull(message = "ID不存在") String id,
--- nas-music/src/main/java/top/itning/yunshunas/music/controller/MusicController.java
+++ nas-music/src/main/java/top/itning/yunshunas/music/controller/MusicController.java
@@ -1,6 +1,7 @@
 package top.itning.yunshunas.music.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -10,6 +11,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.itning.yunshunas.music.dto.MusicDTO;
 import top.itning.yunshunas.music.dto.MusicMetaInfo;
 import top.itning.yunshunas.music.dto.RestModel;
 import top.itning.yunshunas.music.service.FileService;
@@ -19,6 +21,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 /**
+ * 音乐相关接口
+ *
  * @author itning
  * @date 2020/9/5 11:25
  */
@@ -35,31 +39,64 @@ public class MusicController {
         this.fileService = fileService;
     }
 
+    /**
+     * 分页获取音乐列表
+     *
+     * @param page 分页信息
+     * @return 音乐列表
+     */
     @GetMapping
-    public ResponseEntity<?> getAll(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page) {
+    public ResponseEntity<RestModel<Page<MusicDTO>>> getAll(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page) {
         return RestModel.ok(musicService.findAll(page));
     }
 
+    /**
+     * 搜索音乐和歌手
+     *
+     * @param page    分页信息
+     * @param keyword 关键词
+     * @return 搜索结果
+     */
     @GetMapping("/search")
-    public ResponseEntity<?> search(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page,
-                                    @NotEmpty(message = "关键字不能为空") String keyword) {
+    public ResponseEntity<RestModel<Page<MusicDTO>>> search(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page,
+                                                            @NotEmpty(message = "关键字不能为空") String keyword) {
         return RestModel.ok(musicService.fuzzySearch(keyword, page));
     }
 
+    /**
+     * 搜索歌名
+     *
+     * @param page    分页信息
+     * @param keyword 关键词
+     * @return 搜索结果
+     */
     @GetMapping("/search/name")
-    public ResponseEntity<?> searchName(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page,
-                                        @NotEmpty(message = "关键字不能为空") String keyword) {
+    public ResponseEntity<RestModel<Page<MusicDTO>>> searchName(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page,
+                                                                @NotEmpty(message = "关键字不能为空") String keyword) {
         return RestModel.ok(musicService.fuzzySearchName(keyword, page));
     }
 
+    /**
+     * 搜索歌手
+     *
+     * @param page    分页信息
+     * @param keyword 关键词
+     * @return 搜索结果
+     */
     @GetMapping("/search/singer")
-    public ResponseEntity<?> searchSinger(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page,
-                                          @NotEmpty(message = "关键字不能为空") String keyword) {
+    public ResponseEntity<RestModel<Page<MusicDTO>>> searchSinger(@PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable page,
+                                                                  @NotEmpty(message = "关键字不能为空") String keyword) {
         return RestModel.ok(musicService.fuzzySearchSinger(keyword, page));
     }
 
+    /**
+     * 获取歌曲元信息，包括歌手歌名和封面
+     *
+     * @param id 歌曲ID
+     * @return 元信息
+     */
     @GetMapping("/metaInfo")
-    public ResponseEntity<?> metaInfo(@NotNull(message = "ID不存在") String id) {
+    public ResponseEntity<RestModel<MusicMetaInfo>> metaInfo(@NotNull(message = "ID不存在") String id) {
         MusicMetaInfo metaInfo = fileService.getMusicMetaInfo(id);
         if (null != metaInfo && !CollectionUtils.isEmpty(metaInfo.getCoverPictures())) {
             metaInfo.getCoverPictures().forEach(item -> item.setBinaryData(null));
