65d836afbceafd108e7ccec62422fc1ed45a905f
itning <itning@itning.top>
2021-10-16 12:18:58 +0800

    feat: 基础认证

--- nas-common/src/main/java/top/itning/yunshunas/common/config/NasProperties.java
+++ nas-common/src/main/java/top/itning/yunshunas/common/config/NasProperties.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * Email 配置
  *
@@ -33,4 +35,24 @@ public class NasProperties {
      * 歌词文件目录
      */
     private String lyricFileDir;
+
+    /**
+     * 开启基础认证
+     */
+    private boolean enableBasicAuth;
+
+    /**
+     * 基础认证用户名
+     */
+    private String basicAuthUsername;
+
+    /**
+     * 基础认证密码
+     */
+    private String basicAuthPassword;
+
+    /**
+     * 基础认证忽略路径
+     */
+    private List<String> ignorePath;
 }
--- nas-music/src/main/java/top/itning/yunshunas/music/service/UploadService.java
+++ nas-music/src/main/java/top/itning/yunshunas/music/service/UploadService.java
@@ -7,7 +7,20 @@ import org.springframework.web.multipart.MultipartFile;
  * @since 2021/10/16 11:33
  */
 public interface UploadService {
+    /**
+     * 上传音乐
+     *
+     * @param file 文件
+     * @throws Exception 上传出错
+     */
     void uploadMusic(MultipartFile file) throws Exception;
 
+    /**
+     * 上传歌词
+     *
+     * @param musicId 歌词对应的歌曲ID
+     * @param file    文件
+     * @throws Exception 上传出错
+     */
     void uploadLyric(String musicId, MultipartFile file) throws Exception;
 }
