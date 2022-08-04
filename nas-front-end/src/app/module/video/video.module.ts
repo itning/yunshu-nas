762f53b1bfaef5f924885ab699ca3006e19dbd22
itning <itning@itning.top>
2022-08-04 19:19:15 +0800

    feat: add video module

---
+++ nas-front-end/src/app/module/video/video.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SharedModule} from "../shared/shared.module";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class VideoModule {
+}
