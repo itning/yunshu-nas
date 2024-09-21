6a2cd2027625809136fd134db7f5f68596802dd5
itning <itning@itning.top>
2024-09-21 17:55:49 +0800

    feat: support https

--- nas-front-end/src/app/service/music.service.ts
+++ nas-front-end/src/app/service/music.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Page} from "../http/model/page/Page";
 import {Music} from "../http/model/Music";
 import {NzTableQueryParams} from "ng-zorro-antd/table";
 import {map} from "rxjs/operators";
@@ -11,20 +10,21 @@ import {environment} from "../../environments/environment";
   providedIn: 'root'
 })
 export class MusicService {
+  private readonly isHttps: boolean = window.location.protocol === 'https:';
 
   constructor(private http: HttpClient) {
   }
 
   getMusicList(): Observable<Music[]> {
-    return this.http.get<Music[]>(environment.backEndUrl + "/api/music/list");
+    return this.http.get<Music[]>(environment.backEndUrl + "/api/music/list").pipe(this.replaceMusicListUriIfNeed());
   }
 
   searchMusic(keyword: string, queryParams: NzTableQueryParams): Observable<Music[]> {
-    return this.http.get<Music[]>(environment.backEndUrl + "/api/music/list/search" + this.convertQueryParameters(queryParams, `keyword=${keyword}`));
+    return this.http.get<Music[]>(environment.backEndUrl + "/api/music/list/search" + this.convertQueryParameters(queryParams, `keyword=${keyword}`)).pipe(this.replaceMusicListUriIfNeed());
   }
 
   getOneMusic(musicId: string): Observable<Music> {
-    return this.http.get<Music>(environment.backEndUrl + "/api/music/id/" + musicId);
+    return this.http.get<Music>(environment.backEndUrl + "/api/music/id/" + musicId).pipe(this.replaceMusicUriIfNeed());
   }
 
   getLyricFromUrl(url: string): Observable<string> {
@@ -32,11 +32,11 @@ export class MusicService {
   }
 
   editMusic(formData: FormData): Observable<Music> {
-    return this.http.post<Music>(environment.backEndUrl + "/api/music/edit", formData);
+    return this.http.post<Music>(environment.backEndUrl + "/api/music/edit", formData).pipe(this.replaceMusicUriIfNeed());
   }
 
   addMusic(formData: FormData): Observable<Music> {
-    return this.http.post<Music>(environment.backEndUrl + "/api/music/add", formData);
+    return this.http.post<Music>(environment.backEndUrl + "/api/music/add", formData).pipe(this.replaceMusicUriIfNeed());
   }
 
   delMusic(musicId: string): Observable<any> {
@@ -64,5 +64,43 @@ export class MusicService {
       customQueryParams = '';
     }
     return `?${customQueryParams}page=${queryParams.pageIndex - 1}&size=${queryParams.pageSize}${sortQueryParams}`;
+  }
+
+  private replaceMusicListUriIfNeed() {
+    return map((musicList: Music[]) => {
+      if (this.isHttps) {
+        return musicList.map((music: Music) => {
+          if (music.coverUri && music.coverUri.startsWith('http://')) {
+            music.coverUri = music.coverUri.replace('http://', 'https://');
+          }
+          if (music.lyricUri && music.lyricUri.startsWith('http://')) {
+            music.lyricUri = music.lyricUri.replace('http://', 'https://');
+          }
+          if (music.musicUri && music.musicUri.startsWith('http://')) {
+            music.musicUri = music.musicUri.replace('http://', 'https://');
+          }
+          return music;
+        });
+      }
+      return musicList;
+    });
+  }
+
+  private replaceMusicUriIfNeed() {
+    return map((music: Music) => {
+      if (this.isHttps) {
+        if (music.coverUri && music.coverUri.startsWith('http://')) {
+          music.coverUri = music.coverUri.replace('http://', 'https://');
+        }
+        if (music.lyricUri && music.lyricUri.startsWith('http://')) {
+          music.lyricUri = music.lyricUri.replace('http://', 'https://');
+        }
+        if (music.musicUri && music.musicUri.startsWith('http://')) {
+          music.musicUri = music.musicUri.replace('http://', 'https://');
+        }
+        return music;
+      }
+      return music;
+    });
   }
 }
