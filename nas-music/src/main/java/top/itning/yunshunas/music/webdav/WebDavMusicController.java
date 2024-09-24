c7426a9ab8d1cb60e73b627d631793d108e87658
itning <itning@itning.top>
2024-09-24 21:23:09 +0800

    feat: webdav support

--- nas-music/src/main/java/top/itning/yunshunas/music/webdav/WebDavMusicController.java
+++ nas-music/src/main/java/top/itning/yunshunas/music/webdav/WebDavMusicController.java
@@ -22,9 +22,9 @@ public class WebDavMusicController {
         this.fileService = fileService;
     }
 
-    @GetMapping("{name}.{ext}")
-    public void webDavIndex(@PathVariable String name, @PathVariable String ext, @RequestHeader(required = false) String range, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        log.info("get name: {} ext: {}", name, ext);
+    @GetMapping("/{name}-{single}.{ext}")
+    public void webDavIndex(@PathVariable String name, @PathVariable String single, @PathVariable String ext, @RequestHeader(required = false) String range, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        log.info("get name: {} single: {} ext: {}", name, single, ext);
         fileService.getOneMusic("277d5e1275a04e0299b635dba7c0ecb7", range, request, response);
     }
 }
